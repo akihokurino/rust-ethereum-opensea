4292051d0e328e0d245a12fe19263eb508e8a8e1
akihokurino <aki030402@gmail.com>
2022-11-09 18:22:49 +0900

    fixed

--- src/command/deploy.rs
+++ src/command/deploy.rs
@@ -5,10 +5,12 @@ use crate::model::Schema;
 pub async fn deploy_contract(schema: Schema) -> CliResult<()> {
     match schema {
         Schema::ERC721 => {
+            println!("{}", "deploy erc721 contract.........");
             let erc721_cli = erc721::Client::new();
             erc721_cli.deploy().await?
         }
         Schema::ERC1155 => {
+            println!("{}", "deploy erc1155 contract.........");
             let erc1155_cli = erc1155::Client::new();
             erc1155_cli.deploy().await?
         }
